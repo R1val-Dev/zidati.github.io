5b5a975cccd3e75cb3576dc153c877809f700850
Zidati <bike01@rogers.com>
2016-11-17 23:32:47 -0500

    added everything back

---
+++ packages.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+dpkg-deb -bZgzip projects/3dotspringy debs
+# dpkg-deb -bZgzip projects/<project name> <outputlocation>
+dpkg-deb -bZgzip projects/androidmarshmallowspringy debs
+# dpkg-deb -bZgzip projects/<project name> <outputlocation>
+dpkg-deb -bZgzip projects/atomspringy debs
+# dpkg-deb -bZgzip projects/<project name> <outputlocation>
+dpkg-deb -bZgzip projects/ballspringy debs
+# dpkg-deb -bZgzip projects/<project name> <outputlocation>
+dpkg-deb -bZgzip projects/bluespringy debs
+# dpkg-deb -bZgzip projects/<project name> <outputlocation>
+dpkg-deb -bZgzip projects/bouncingdotsspringy debs
+# dpkg-deb -bZgzip projects/<project name> <outputlocation>
+dpkg-deb -bZgzip projects/circleceptionspringy debs
+# dpkg-deb -bZgzip projects/<project name> <outputlocation>
+dpkg-deb -bZgzip projects/circledotsspringy debs
+# dpkg-deb -bZgzip projects/<project name> <outputlocation>
+dpkg-deb -bZgzip projects/circularwavespringy debs
+# dpkg-deb -bZgzip projects/<project name> <outputlocation>
+dpkg-deb -bZgzip projects/colorfulapplespringy debs
+# dpkg-deb -bZgzip projects/<project name> <outputlocation>
+dpkg-deb -bZgzip projects/colorwheelspringy debs
+# dpkg-deb -bZgzip projects/<project name> <outputlocation>
+dpkg-deb -bZgzip projects/cracksspringy debs
+# dpkg-deb -bZgzip projects/<project name> <outputlocation>
+dpkg-deb -bZgzip projects/crispydark debs
+# dpkg-deb -bZgzip projects/<project name> <outputlocation>
+dpkg-deb -bZgzip projects/crystalballspringy debs
+# dpkg-deb -bZgzip projects/<project name> <outputlocation>
+dpkg-deb -bZgzip projects/cubesspringy debs
+# dpkg-deb -bZgzip projects/<project name> <outputlocation>
+dpkg-deb -bZgzip projects/donutsspringy debs
+# dpkg-deb -bZgzip projects/<project name> <outputlocation>
+dpkg-deb -bZgzip projects/downloadingerrorspringy debs
+# dpkg-deb -bZgzip projects/<project name> <outputlocation>
+dpkg-deb -bZgzip projects/fluidspringy debs
+# dpkg-deb -bZgzip projects/<project name> <outputlocation>
+dpkg-deb -bZgzip projects/gothamcomplete debs
+# dpkg-deb -bZgzip projects/<project name> <outputlocation>
+dpkg-deb -bZgzip projects/guitarbunnyspringy debs
+# dpkg-deb -bZgzip projects/<project name> <outputlocation>
+dpkg-deb -bZgzip projects/hexagondotsspringy debs
+# dpkg-deb -bZgzip projects/<project name> <outputlocation>
+dpkg-deb -bZgzip projects/hexagonspringy debs
+# dpkg-deb -bZgzip projects/<project name> <outputlocation>
+dpkg-deb -bZgzip projects/knotspringy debs
+# dpkg-deb -bZgzip projects/<project name> <outputlocation>
+dpkg-deb -bZgzip projects/leftrightspringy debs
+# dpkg-deb -bZgzip projects/<project name> <outputlocation>
+dpkg-deb -bZgzip projects/liverbirdlockglyph debs
+# dpkg-deb -bZgzip projects/<project name> <outputlocation>
+dpkg-deb -bZgzip projects/majesticwhalesspringy debs
+# dpkg-deb -bZgzip projects/<project name> <outputlocation>
+dpkg-deb -bZgzip projects/martianspringy debs
+# dpkg-deb -bZgzip projects/<project name> <outputlocation>
+dpkg-deb -bZgzip projects/particlesheadspringy debs
+# dpkg-deb -bZgzip projects/<project name> <outputlocation>
+dpkg-deb -bZgzip projects/pinkretardspringy debs
+# dpkg-deb -bZgzip projects/<project name> <outputlocation>
+dpkg-deb -bZgzip projects/pokemongospringy debs
+# dpkg-deb -bZgzip projects/<project name> <outputlocation>
+dpkg-deb -bZgzip projects/prismaticheadspringy debs
+# dpkg-deb -bZgzip projects/<project name> <outputlocation>
+dpkg-deb -bZgzip projects/rotating8springy debs
+# dpkg-deb -bZgzip projects/<project name> <outputlocation>
+dpkg-deb -bZgzip projects/rotatingspherespringy debs
+# dpkg-deb -bZgzip projects/<project name> <outputlocation>
+dpkg-deb -bZgzip projects/sixpointedstarspringy debs
+# dpkg-deb -bZgzip projects/<project name> <outputlocation>
+dpkg-deb -bZgzip projects/spaghettispringy debs
+# dpkg-deb -bZgzip projects/<project name> <outputlocation>
+dpkg-deb -bZgzip projects/spheresignalspringy debs
+# dpkg-deb -bZgzip projects/<project name> <outputlocation>
+dpkg-deb -bZgzip projects/spinningcubespringy debs
+# dpkg-deb -bZgzip projects/<project name> <outputlocation>
+dpkg-deb -bZgzip projects/spinningspherespringy debs
+# dpkg-deb -bZgzip projects/<project name> <outputlocation>
+dpkg-deb -bZgzip projects/spiralhexagon debs
+# dpkg-deb -bZgzip projects/<project name> <outputlocation>
+dpkg-deb -bZgzip projects/ticktockspringy debs
+# dpkg-deb -bZgzip projects/<project name> <outputlocation>
+dpkg-deb -bZgzip projects/trippyspringy debs
+# dpkg-deb -bZgzip projects/<project name> <outputlocation>
+dpkg-deb -bZgzip projects/twirl2springy debs
+# dpkg-deb -bZgzip projects/<project name> <outputlocation>
+dpkg-deb -bZgzip projects/watchdogslockglyph debs
+# dpkg-deb -bZgzip projects/<project name> <outputlocation>
+dpkg-deb -bZgzip projects/wifispringy debs
+# dpkg-deb -bZgzip projects/<project name> <outputlocation>
+dpkg-deb -bZgzip projects/wormsspringy debs
+# dpkg-deb -bZgzip projects/<project name> <outputlocation>
